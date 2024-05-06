0ec305b6112cfed6db41b79d08e540fe36894b5d
Eduardo Moraton <moraton.eduardo@gmail.com>
2024-05-06 23:33:09 +0200

    With window location

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,32 +1,33 @@
 'use client'
 import DataLine from "@/components/dataLine";
-import Image from "next/image";
-import { useSearchParams } from 'next/navigation'
-import { Suspense, useEffect, useState } from "react";
-
+import { useEffect, useState } from "react";
 
 export default function Result() {
-  const [lines, setLines] = useState([""])
-  const searchParams = useSearchParams()
+  const [lines, setLines] = useState(["",""]);
+
+  const fetchData = () => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const nifnie = searchParams.get("nifnie");
+    const fechaExamen = searchParams.get("fechaExamen");
+    const clasePermiso = searchParams.get("clasepermiso");
+    const fechaNacimiento = searchParams.get("fechaNacimiento");
+
+    fetch(`/api/getResult?nifnie=${nifnie}&fechaExamen=${fechaExamen}&clasePermiso=${clasePermiso}&fechaNacimiento=${fechaNacimiento}`)
+      .then((res) => res.json())
+      .then((jsonData) => {
+        console.log(jsonData);
+    
+        setLines(JSON.parse(jsonData))
+        
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }
 
   useEffect(() => {
-    const fetchData = async () => {
-      const nifnie = searchParams.get("nifnie")
-      const fechaExamen = searchParams.get("fechaExamen")
-      const clasePermiso = searchParams.get("clasepermiso")
-      const fechaNacimiento = searchParams.get("fechaNacimiento")
-
-      try {
-        const res = await fetch(`api/getResult?nifnie=${nifnie}&fechaExamen=${fechaExamen}&clasePermiso=${clasePermiso}&fechaNacimiento=${fechaNacimiento}`)
-        const jsonData = await res.json()
-        setLines(JSON.parse(jsonData))
-      } catch (error) {
-        console.error('Error fetching data:', error)
-      }
-    }
-
-    fetchData()
-  }, [searchParams])
+    fetchData();
+  }, []);
 
   const handleShare = () => {
     if (navigator.share) {
@@ -35,7 +36,7 @@ export default function Result() {
         url: window.location.href
       })
       .then(() => console.log('Successful share'))
-      .catch((error) => console.log('Error sharing:', error));
+      .catch((error) => console.error('Error sharing:', error));
     } else {
       const el = document.createElement('textarea');
       el.value = window.location.href;
@@ -48,18 +49,17 @@ export default function Result() {
   }
 
   return (
-    <Suspense fallback={'<>Loading...</>'}>
-      <main className="flex items-start justify-center w-screen h-screen pt-10">
-        <div className={'flex flex-col gap-3 m-10'}>
-          {
-            lines.length > 2 ?
-            lines.map(((line, i)=>(
-              <DataLine key={i} label={"Linea de datos:"} data={line}></DataLine>
-            ))) : <div>No hay resultados</div>
-          }
-          <button onClick={handleShare} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Share</button>
-        </div>
-      </main>
-    </Suspense>
+    <main className="flex items-start justify-center w-screen h-screen pt-10">
+      <div className={'flex flex-col gap-3 m-10'}>
+        {lines.length > 0 ? (
+          lines.map((line, i) => (
+            <DataLine key={i} label={"Linea de datos:"} data={line}></DataLine>
+          ))
+        ) : (
+          <div>No hay resultados</div>
+        )}
+        <button onClick={handleShare} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Share</button>
+      </div>
+    </main>
   );
 }
